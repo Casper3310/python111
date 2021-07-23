8989172c976d5deaf5f3b9855025ffbfbcd790ae
Casper3310 <61928735+Casper3310@users.noreply.github.com>
2021-07-23 13:53:01 +0800

    no message

--- app.py
+++ app.py
@@ -10,39 +10,41 @@ import time
 # 22185江秋遠 28138趙珮伶 28131周俊佑 20941沈旻頡 26566張智傑 26901朱宇凡 21288尤耀堂
 ID_list = ["28129", "28135", "28127", "22214", "21513", "28399",
            "28133", "28399", "22185", "28138", "28131", "20941", "26566", "26901", "21288"]
-for empID in ID_list:
-    driver = webdriver.Chrome()
-    driver.get("https://trtccod.app/covid-health-report/")
-    element = driver.find_elements_by_tag_name('button')[1]
-    element.click()
-    #WebDriverWait(driver, 等待的最長時間, 檢查條件的頻率, 忽略的例外類別).until(expected_conditions條件, 超時例外的錯誤訊息)
+for eee in range(0, 2):
+    for empID in ID_list:
+        driver = webdriver.Chrome()
+        driver.get("https://trtccod.app/covid-health-report/")
+        element = driver.find_elements_by_tag_name('button')[1]
+        element.click()
+        #WebDriverWait(driver, 等待的最長時間, 檢查條件的頻率, 忽略的例外類別).until(expected_conditions條件, 超時例外的錯誤訊息)
 
-    locator = (By.ID, "empid")
-    inputID = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(locator))
-    inputID.send_keys(empID)
-    NextStep = driver.find_elements_by_class_name('btn-primary')[1]
-    NextStep.click()
-    locator = (By.CSS_SELECTOR, '.btn-sm')
-    temp = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(locator))
-    # 按鍵數字
-    # print(temp[0].text)   35
-    # print(temp[1].text)   36
-    # print(temp[6].text)   0.1
-    # print(temp[14].text)  0.9
-    index = random.randint(0, 1)
-    temp[index].click()
-    index = random.randint(6, 14)
-    temp[index].click()
+        locator = (By.ID, "empid")
+        inputID = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(locator))
+        inputID.send_keys(empID)
+        NextStep = driver.find_elements_by_class_name('btn-primary')[1]
+        NextStep.click()
+        locator = (By.CSS_SELECTOR, '.btn-sm')
+        temp = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(locator))
+        # 按鍵數字
+        # print(temp[0].text)   35
+        # print(temp[1].text)   36
+        # print(temp[6].text)   0.1
+        # print(temp[14].text)  0.9
+        index = random.randint(0, 1)
+        temp[index].click()
+        index = random.randint(6, 14)
+        temp[index].click()
 
-    js = "document.documentElement.scrollTop=10000"
-    driver.execute_script(js)
-    time.sleep(1)
-    healthy = driver.find_element_by_id('healthy')
-    healthy.click()
-    submit = driver.find_element_by_class_name('btn-primary')
-    submit.click()
-    driver.close()
-    print(empID)
-print(ID_list)
+        js = "document.documentElement.scrollTop=10000"
+        driver.execute_script(js)
+        time.sleep(1)
+        healthy = driver.find_element_by_id('healthy')
+        healthy.click()
+        submit = driver.find_element_by_class_name('btn-primary')
+        submit.click()
+        driver.close()
+        print(empID)
+    print(ID_list)
+print("結束")
